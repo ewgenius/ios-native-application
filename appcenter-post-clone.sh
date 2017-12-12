e66b3bca2d30e10342195c8b2a951834aabf9f23
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-12-12 22:55:38 +0300

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,1 +1,2 @@
-echo "APPCENTER_BUILD_ID = $APPCENTER_BUILD_ID"
+echo "APPCENTER_BUILD_ID = $APPCENTER_BUILD_ID" # this is working
+echo "APPCENTER_BUILD_ID = $(APPCENTER_BUILD_ID)" # this is not working

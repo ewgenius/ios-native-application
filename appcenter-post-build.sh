d579d5d2d50365a69e4b8330792f9ad95cffec71
Evgeniy Khramkov <ewgeniux@gmail.com>
2018-02-16 22:51:27 +0300

    Update and rename appcenter-post-clone.sh to appcenter-post-build.sh

---
+++ appcenter-post-build.sh
@@ -0,0 +1,1 @@
+curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"webhookbot\", \"text\": \"New Build just finished $(APPCENTER_BUILD_ID).\", \"icon_emoji\": \":ghost:\"}" $SLACK_WEBHOOK
--- appcenter-post-clone.sh
+++
@@ -1,2 +0,0 @@
-echo "APPCENTER_BUILD_ID = $APPCENTER_BUILD_ID" # this is working
-echo "APPCENTER_BUILD_ID = $(APPCENTER_BUILD_ID)" # this is not working
